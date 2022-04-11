288256ef6e4d55cf46b0c0322f1251b742581272
Michael Schwarz <michael.schwarz@hcl.com>
2022-04-11 15:46:45 -0500

    Workpoint

---
+++ src/mkbfgroup.py
@@ -0,0 +1,65 @@
+import argparse
+import getpass
+import requests
+
+print("Hello")
+
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument(
+    "-s", "--bfserver", type=str, help="BigFix REST Server name/IP address", required=True
+)
+parser.add_argument(
+    "-p", "--bfport", type=int, help="BigFix Port number (default 52311)", default=52311
+)
+parser.add_argument("-U", "--bfuser", type=str, help="BigFix Console/REST User name", required=True)
+parser.add_argument("-P", "--bfpass", type=str, help="BigFix Console/REST Password (will prompt if omitted)")
+parser.add_argument("-r", "--relevance", type=str, help="Relevance that defines the group". required=True)
+parser.add_argument("-c", "--customsite", type=str, help="Custom site name", required=True)
+parser.add_argument("-t", "--title", type=str, required=True)
+
+conf = parser.parse_args()
+
+if conf.bfpass is None:
+    ## TODO: Modify to prompt until 2 consecutive matching passwords
+    passwd = getpass.getpass(prompt="Enter REST API password:")
+else:
+    passwd = conf.bfpass
+
+postXml = f'''\
+<BES xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="BES.xsd">
+<ComputerGroup>
+<Title>{conf.title}</Title>
+<Domain>BESC</Domain>
+<JoinByIntersection>true</JoinByIntersection>
+<SearchComponentRelevance Comparison="IsTrue">
+<Relevance>{conf.relevance}</Relevance>
+</SearchComponentRelevance>
+</ComputerGroup>
+</BES>
+'''.strip()
+
+session = requests.Session()
+session.auth = (conf.bfuser, passwd)
+
+qheader = {
+    'Content-Type' : 'application/x-www-form-urlencoded'
+}
+
+sitename = requests.utils.quote(conf.customsite)
+
+req = requests.Request('POST'
+    , f"https://{conf.bfserver}:{conf.bfport}/api/computergroups/custom/{sitename}"
+    , headers=qheader
+    , data=xmldata
+)
+
+prepped = session.prepare_request(req)
+
+result = session.send(prepped, verify = False)
+
+if result.status_code < 200 or result.status_code >= 300:
+    print(f"REST API authentication failed with status {result.status_code}")
+    print(f"Reason: {result.text}")
+
